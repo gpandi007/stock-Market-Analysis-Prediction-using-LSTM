d9bd9c8dac9d859705f8aa498c3641185535cc40
gpandi007 <48507358+gpandi007@users.noreply.github.com>
2022-06-29 16:06:16 +0530

    stock Market Analysis 📈 + Prediction using LSTM
    stock Market Analysis 📈 + Prediction using LSTM

---
+++ stock Market Analysis 📈 + Prediction using LSTM.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# https://www.kaggle.com/code/mahwiz/stock-market-prediction
+
+
+# In[7]:
+
+
+import pandas as pd 
+import numpy as np
+import datetime as dt
+import pandas_datareader as web
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, LSTM
+
+
+# In[43]:
+
+
+# fetching data from Yahoo Finance
+company = '^NSEI'
+
+
+# In[44]:
+
+
+start = dt.datetime(2004, 1, 1)
+end = dt.datetime(2022, 6, 28)
+
+data = web.DataReader(company, 'yahoo', start, end)
+
+#Scale data
+scaler = MinMaxScaler(feature_range=(0,1))
+scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1,1))
+prediction_days = 60
+x_train = []
+y_train = []
+
+for x in range(prediction_days, len(scaled_data)):
+    x_train.append(scaled_data[x-prediction_days:x, 0])
+    y_train.append(scaled_data[x, 0])
+    
+x_train, y_train = np.array(x_train), np.array(y_train)
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1],1))
+
+
+# In[45]:
+
+
+data
+
+
+# In[46]:
+
+
+#build the Model
+
+model = Sequential()
+model.add(LSTM(units=50, return_sequences=True,input_shape=(x_train.shape[1], 1)))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50, return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50))
+model.add(Dropout(0.2))
+model.add(Dense(units=1)) #prediction of the next closing value
+
+model.compile(optimizer='adam', loss='mean_squared_error')
+model.fit(x_train, y_train, epochs=25, batch_size=32)
+
+
+# In[47]:
+
+
+# Testing the model accuracy on existing data
+# load Test Data
+test_start = dt.datetime(2021, 1, 1)
+test_end = dt.datetime.now()
+test_data = web.DataReader(company, 'yahoo', test_start, test_end)
+actual_prices = test_data['Close'].values
+total_dataset = pd.concat((data['Close'], test_data['Close']), axis=0)
+model_inputs = total_dataset[len(total_dataset) - len(test_data) - prediction_days:].values
+model_inputs = model_inputs.reshape(-1,1)
+model_inputs = scaler.transform(model_inputs)
+
+
+# In[48]:
+
+
+# Make prediction on test data
+x_test = []
+
+for x in range(prediction_days, len(model_inputs)):
+    x_test.append(model_inputs[x-prediction_days:x, 0])
+    
+x_test = np.array(x_test)
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+predicted_prices = model.predict(x_test)
+predicted_prices = scaler.inverse_transform(predicted_prices)
+# Plot the test prediction
+plt.figure(figsize=(12,8))
+plt.plot(actual_prices, color='blue', label=f'Actual {company} Price')
+plt.plot(predicted_prices, color='red', label=f'Predicted {company} Price')
+plt.title(f'{company} Share Price')
+plt.xlabel('Time')
+plt.ylabel(f'{company} Share Price')
+plt.legend()
+plt.show()
+
+
+# In[49]:
+
+
+# Predict Next day
+real_data = [model_inputs[len(model_inputs) + 1 - prediction_days:len(model_inputs+1), 0]]
+real_data = np.array(real_data)
+real_data = np.reshape(real_data, (real_data.shape[0], real_data.shape[1], 1))
+
+prediction = model.predict(real_data)
+prediction = scaler.inverse_transform(prediction)
+print(f'Prediction : {prediction}')
+
+
+# In[ ]:
+
+
+
+
